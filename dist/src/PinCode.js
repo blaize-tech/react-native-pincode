316194442563a1f91072fd84d9c3f7aa84bf62a5
newcodepusher <svyatoslav.d@blaize.tech>
2020-11-17 17:35:51 +0200

    test empty column component

--- dist/src/PinCode.js
+++ dist/src/PinCode.js
@@ -383,7 +383,9 @@ class PinCode extends React.PureComponent {
                             // this.props.styleEmptyColumn
                             styles.colButtonCircle,
                             this.props.styleColumnButtons
-                        ] }, this.props.emptyColumnComponent(this.props.launchTouchID)),
+                        ] }, this.props.emptyColumnComponent
+                        ? this.props.emptyColumnComponent(this.props.launchTouchID)
+                        : React.createElement(react_native_1.Text, null, "Empty")),
                     React.createElement(react_native_easy_grid_1.Col, { style: [
                             styles.colButtonCircle,
                             this.props.styleColumnButtons
